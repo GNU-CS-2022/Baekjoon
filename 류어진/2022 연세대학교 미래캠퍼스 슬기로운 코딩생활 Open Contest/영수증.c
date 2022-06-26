f87562ddb40fe1dbf65932984a043f346727862e
Ryueojin <dbdjwls3137@naver.com>
2022-06-26 13:10:57 +0900

    Update 영수증.c

--- 류어진/2022 연세대학교 미래캠퍼스 슬기로운 코딩생활 Open Contest/영수증.c
+++ 류어진/2022 연세대학교 미래캠퍼스 슬기로운 코딩생활 Open Contest/영수증.c
@@ -1,12 +1,5 @@
-//아직 수정중
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-struct all {
-	int one_money[1000001];
-	int one_num[11];
-};
-
 
 int main()
 {
@@ -18,15 +11,15 @@ int main()
 	scanf_s("%d", &num);
 	num >= 0 && num <= 100;
 
-	struct all one;
-	for (int i = 0; i < num; i++)
-	{
-		scanf_s("%s %s", one.one_money, one.one_num);
-	}
+
 	int sum = 0;
 	for (int i = 0; i < num; i++)
 	{
-		sum = sum +(one.one_money[i] * one.one_num[i]);
+		int one_money, one_num;
+		scanf_s("%d %d", &one_money, &one_num);
+		one_money >= 0 && one_num <= 1000000;
+		one_num >= 0 && one_num <= 10;
+		sum = sum +(one_money*one_num);
 	}
 	if (sum_money == sum)
 	{
