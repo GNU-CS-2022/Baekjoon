3c3db45cefcc1fea2764d0eff654909a2acdb5c2
Ryueojin <dbdjwls3137@naver.com>
2022-07-07 20:05:36 +0900

    Create 평균은 넘겠지.c

---
+++ 류어진/일반/평균은 넘겠지.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+	scanf_s("%d", &c);
+	int d;
+	for (int i = 0; i < c; i++)
+	{
+		float sum = 0, avg = 0;
+		int score[101] = { 0, };
+		int count = 0;
+		scanf_s("%d", &d);
+		for (int j = 0; j < d;j++)
+		{
+			scanf_s("%d", &score[j], sizeof(score[j]));
+			sum += (float)score[j];
+		}
+		avg = sum / (float)d;
+		for (int j = 0; j < d; j++)
+		{
+			if (score[j]>avg)
+			{
+				count++;
+			}
+		}
+		printf("%.3f \n", ((float)count / (float)d)*100);
+	}
+	return 0;
+}
